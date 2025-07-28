2119ebc5ccbacfc35cba7317661b3db59e2fb4da
MayHua59 <mayhuadev59@gmail.com>
2025-07-28 09:00:31 +0630

    comment News Sections in home page

--- src/app/mm/page.js
+++ src/app/mm/page.js
@@ -25,8 +25,8 @@ const Page = () => {
       <MyanmarInPicturesSection />
       <CallActionSection />
       <TravelerInformationSection />
-      <TourismNewsSection />
-      <LatestNewsSection />
+      {/* <TourismNewsSection />
+      <LatestNewsSection /> */}
     </>
   );
 };
