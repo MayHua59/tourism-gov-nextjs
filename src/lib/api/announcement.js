cbfe18ef6a65b58103d0419cc3a619c98726fd38
MayHua59 <mayhuadev59@gmail.com>
2025-08-06 16:51:28 +0630

    change api for announcement

--- src/lib/api/announcement.js
+++ src/lib/api/announcement.js
@@ -6,7 +6,7 @@ export async function fetchAnnouncementList() {
   const res = await fetch(ANNOUNCEMENT_API_URL, { cache: "no-store" });
   if (!res.ok) throw new Error("Failed to fetch announcements list");
   return await res.json();
-}
+} 
 
 export async function fetchAnnouncementDetail(slug) {
   const res = await fetch(ANNOUNCEMENT_API_URL(slug), { cache: "no-store" });
