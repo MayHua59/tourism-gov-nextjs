8d4a913445d86e4b775e8c98e72ee802c2351e38
MayHua59 <mayhuadev59@gmail.com>
2025-09-15 12:14:11 +0630

    change cover_photo_url into cover_photo

--- src/app/en/festivals/page.js
+++ src/app/en/festivals/page.js
@@ -74,7 +74,7 @@ export default function FestivalsPage() {
               >
                 <div className={styles.festivalImageWrapper}>
                   <img
-                    src={festival.cover_photo_url}
+                    src={festival.cover_photo}
                     alt={festival.name}
                     className={styles.festivalImg}
                   />
